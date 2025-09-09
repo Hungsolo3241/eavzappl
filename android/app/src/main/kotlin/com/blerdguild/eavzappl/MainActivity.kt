f7f26df71ee13b1008431a85aa54514905c98192
Hungsolo3241 <kagiso.mako@gmail.com>
2025-09-09 20:56:57 +0200

    This version of the code DOES NOT have appwide screenshot prevention. Removed to allow casting to physical while we continue to build. Remember to reinclude prevention before launch

--- android/app/src/main/kotlin/com/blerdguild/eavzappl/MainActivity.kt
+++ android/app/src/main/kotlin/com/blerdguild/eavzappl/MainActivity.kt
@@ -1,12 +1,5 @@
 package com.blerdguild.eavzappl
 
 import io.flutter.embedding.android.FlutterActivity
-import android.os.Bundle // Import Bundle
-import android.view.WindowManager // Import WindowManager
 
-class MainActivity: FlutterActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        window.addFlags(WindowManager.LayoutParams.FLAG_SECURE)
-    }
-}
+class MainActivity : FlutterActivity()
